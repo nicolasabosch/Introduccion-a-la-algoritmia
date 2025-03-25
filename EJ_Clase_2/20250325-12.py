4ca6726f568eba2b1f1941116876da9ba4a2a863
Nicolas <nicolasabosch@gmail.com>
2025-03-25 20:56:41 -0300

    NA20250325-2056

--- EJ_Clase_2/20250325-12.py
+++ EJ_Clase_2/20250325-12.py
@@ -1,24 +1,24 @@
 
 cantDinero=int(input("Ingrese la cantidad de dinero\n"))
 
-Billetes1000 = cantDinero//1000
+billetes1000 = cantDinero//1000
 cantDinero%=1000
 
-Billetes500 = cantDinero//500
+billetes500 = cantDinero//500
 cantDinero%=500
 
-Billetes100= cantDinero//100
+billetes100= cantDinero//100
 cantDinero%=100
 
-Billetes50=cantDinero//50
+billetes50=cantDinero//50
 cantDinero%=50
 
-Billetes10=cantDinero//10
+billetes10=cantDinero//10
 cantDinero%=10
 
-Billetes5=cantDinero//5
+billetes5=cantDinero//5
 cantDinero%=5
 
 billetes1=cantDinero//1
 
-print("son", Billetes1000, "Billetes de 1000 \n",Billetes500, "Billetes de 500:\n",Billetes100,"Billetes de 100\n", Billetes50, "Billetes de 50: \n",Billetes10, "Billetes de 10:\n",Billetes5,"Billetes de 5 \n", billetes1 ,"Billetes de 1")
+print("son", billetes1000, "billetes de 1000 \n",billetes500, "billetes de 500:\n",billetes100,"billetes de 100\n", billetes50, "billetes de 50: \n",billetes10, "billetes de 10:\n",billetes5,"billetes de 5 \n", billetes1 ,"billetes de 1")
